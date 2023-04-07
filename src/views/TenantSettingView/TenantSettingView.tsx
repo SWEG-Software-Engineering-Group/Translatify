19d32bcab15926cc5a31248716dd73b7cc246f49
Davide <sgrazzuttidavide@gmail.com>
2023-04-07 12:54:55 +0200

    modificate tabelle tenant settings

--- src/views/TenantSettingView/TenantSettingView.tsx
+++ src/views/TenantSettingView/TenantSettingView.tsx
@@ -1,15 +1,24 @@
 import { Box, Collapse, Grid, IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
 import LayoutWrapper from "../../components/LayoutWrapper/LayoutWrapper";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import { grid } from "../../utils/MUI/gridValues";
+import Tenant from "../../types/Tenant";
+import tenantdata from "../TenantSettingView/testData";
+import TenantListItem from "../../components/TenantList/TenantListItem/TenantListItem";
+import TenantSettingItem from "../../components/TenantSettingList/TenantSettingItem";
+
 
 export default function TenantTextsView() {
 
-    const [open, setOpen] = React.useState(false);
-    const [open2, setOpen2] = React.useState(false);
-    const [open3, setOpen3] = React.useState(false);
+    const [tenants, setTenants] = useState<Tenant[]>([]);
+
+
+    useEffect(() => {
+        setTenants(tenantdata);
+      }, []);
+
 
     return (
         <LayoutWrapper userType="admin">
@@ -29,86 +38,24 @@ export default function TenantTextsView() {
                             <TableHead >
                                 <TableRow>
                                     <TableCell/>
-                                    <TableCell>Tenant Name</TableCell>
+                                    <TableCell align="center">Tenant Name</TableCell>
                                     <TableCell align="center">Main Tenant Language</TableCell>
                                     <TableCell align="center">Assigned Users</TableCell>
                                 </TableRow>
                             </TableHead>
                             <TableBody>
-                                <React.Fragment >
-                                    <TableRow >
-                                        <TableCell>
-                                            <IconButton
-                                                aria-label="expand row"
-                                                size="small"
-                                                onClick={() => setOpen(!open)}>
-                                                {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-                                            </IconButton>
-                                        </TableCell>
-                                        <TableCell >Tenant 1 </TableCell>
-                                        <TableCell align="center">English</TableCell>
-                                        <TableCell align="center">No users assigned</TableCell>
+                                
+                                {tenants.length ? (
+                                    tenants.map((tenant) => (
+                                            <TenantSettingItem tenant={tenant} />
+                                    ))
+                                ) : (
+                                    <TableRow sx={{ display: "flex", justifyContent: "center", my: 3 }}>
+                                        <p>No tenants found</p>
                                     </TableRow>
-                                    <TableRow>
-                                        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
-                                            <Collapse in={open} timeout="auto" unmountOnExit>
-                                                <Box sx={{ margin: 1 }}>
-                                                    <Typography align="center" variant="h6" gutterBottom component="div">
-                                                        User list and buttons to add languages to translate
-                                                    </Typography> 
-                                                </Box>
-                                            </Collapse>
-                                        </TableCell>
-                                    </TableRow>
-                                    <TableRow >
-                                        <TableCell>
-                                            <IconButton
-                                                aria-label="expand row"
-                                                size="small"
-                                                onClick={() => setOpen2(!open2)}>
-                                                {open2 ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-                                            </IconButton>
-                                        </TableCell>
-                                        <TableCell >Tenant 2 </TableCell>
-                                        <TableCell align="center">French</TableCell>
-                                        <TableCell align="center">Assigned Users: 2 </TableCell>
-                                    </TableRow>
-                                    <TableRow>
-                                        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
-                                            <Collapse in={open2} timeout="auto" unmountOnExit>
-                                                <Box sx={{ margin: 1 }}>
-                                                    <Typography align="center" variant="h6" gutterBottom component="div">
-                                                    User list and buttons to add languages to translate
-                                                    </Typography> 
-                                                </Box>
-                                            </Collapse>
-                                        </TableCell>
-                                    </TableRow>
-                                    <TableRow >
-                                        <TableCell>
-                                            <IconButton
-                                                aria-label="expand row"
-                                                size="small"
-                                                onClick={() => setOpen3(!open3)}>
-                                                {open3 ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-                                            </IconButton>
-                                        </TableCell>
-                                        <TableCell >Tenant 3 </TableCell>
-                                        <TableCell align="center">Spanish</TableCell>
-                                        <TableCell align="center">Assigned Users: 1 </TableCell>
-                                    </TableRow>
-                                    <TableRow>
-                                        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
-                                            <Collapse in={open3} timeout="auto" unmountOnExit>
-                                                <Box sx={{ margin: 1 }}>
-                                                    <Typography align="center" variant="h6" gutterBottom component="div">
-                                                    User list and buttons to add languages to translate
-                                                    </Typography> 
-                                                </Box>
-                                            </Collapse>
-                                        </TableCell>
-                                    </TableRow>
-                                </React.Fragment>
+                                )}
+                               
+        
                             </TableBody>
                         </Table>
                     </TableContainer>
--- src/views/TenantSettingView/testData.ts
+++ src/views/TenantSettingView/testData.ts
@@ -1,14 +1,63 @@
+import Tenant from "../../types/Tenant";
 
+const Tenantsettings : Tenant[] = [
 
-const Tenantsettings : string[] = [
-
-    "Tenant 1","Inglese","Nessuno",
-
-    "Tenant 2","Francese","2",
-
-    "Tenant 3","Spagnolo","3",
-
-
+    {
+        id: 1,
+        name: "tenant 1",
+        admin: ["admin"],
+        users: [
+        {
+            username: "admin",
+            password: "password",
+            email: "email@email.it",
+            role: "admin",
+            name: "admin name ",
+            surname: "admin surname",
+        },
+        {
+            username: "pippo",
+            password: "password",
+            email: "email@email.it",
+            role: "user",
+            name: "utente name",
+            surname: "pippo surname",
+        }
+        ],
+        creationDate: new Date("2016-01-04 10:34:23"),
+        token: {
+        name: "token1",
+        idTenant: 1,
+        privileges: ["edit"],
+        value: "1111",
+        },
+        languages: ["English", "Italian"],
+        defaultLanguage: "Italian",
+    },
+    {
+        id: 2,
+        name: "tenant 2",
+        admin: ["admin"],
+        users: [
+        {
+            username: "admin",
+            password: "password",
+            email: "email@email.it",
+            role: "admin",
+            name: "admin name",
+            surname: "admin surname",
+        },
+        ],
+        creationDate: new Date("2016-01-04 10:34:23"),
+        token: {
+        name: "token1",
+        idTenant: 1,
+        privileges: ["edit"],
+        value: "1111",
+        },
+        languages: ["English", "Spanish"],
+        defaultLanguage: "Spanish",
+    },
 ];
 
 export default Tenantsettings;
