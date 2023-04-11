f27a0450b4a74a986b119566d2e624ef6e9cfde0
Andrea Meneghello <andreameneghello828@gmail.com>
2023-04-11 19:17:15 +0200

    complete TenantTextCategories components and fix layout

---
+++ src/components/TextCategoriesList/TextCategoriesList.tsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import TextCategory from "../../types/TextCategory";
+import {Grid, Box} from "@mui/material";
+import TextCategoriesListItem from "./TextCategoriesListItem/TextCategoriesListItem";
+
+type Props = {
+    textCategories: TextCategory[];
+};
+
+export default function TextCategoriesList({ textCategories }: Props) {
+    return (
+        <div>
+            <Box padding={2}>
+                <Grid container spacing={2}>
+                    {textCategories?.map((category : TextCategory) => (
+                        <Grid item xs={12} key={category.idCategory}>
+                            <Box width="100%" marginBottom={2}>
+                                <TextCategoriesListItem category={category} />
+                            </Box>
+                        </Grid>
+                    ))}
+                </Grid>
+            </Box>
+        </div>
+    )
+}
--- src/views/TenantTextCategoriesView/TenantTextCategoriesView.tsx
+++ src/views/TenantTextCategoriesView/TenantTextCategoriesView.tsx
@@ -1,13 +1,71 @@
 import { useEffect, useState } from "react";
 import LayoutWrapper from "../../components/LayoutWrapper/LayoutWrapper";
-
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import { grid } from "../../utils/MUI/gridValues";
+import TextSearch from "../../components/TextSearch/TextSearch";
+import TextCategory from "../../types/TextCategory";
+import TextCategoriesListItem from "../../components/TextCategoriesList/TextCategoriesListItem/TextCategoriesListItem";
+import testData from "./testData";
 
 export default function TenantCategoriesView() {    
+    // HOOKS
+    const [categories, setCategories] =  useState<TextCategory[]>([]);
+    const [filteredCategories, setFilteredCategories] = useState<TextCategory[]>([]);
+
+    useEffect(() => {
+        setCategories(testData);
+        setFilteredCategories(testData);
+    }, []);
+
+    const handleSearch = (query: string) => {
+        const filtered = categories.filter((category) => {
+          // cerca il tenant in base al nome
+          if (category.idCategory.toLowerCase().includes(query.toLowerCase())) {
+            return true;
+          }
     
-    
-    return (
-        <LayoutWrapper userType="admin">
-            <h1>Pagina per categorie - to do</h1>
-        </LayoutWrapper>
-    )
+          return false;
+        });
+        setFilteredCategories(filtered);
+      };
+
+    // UI
+  return (
+     <LayoutWrapper userType="admin">
+        <Grid
+            container
+            spacing={1}
+            direction="column"        
+        >
+        <Grid item xs={grid.fullWidth} textAlign={"center"}>
+            <Typography variant="h4">
+                Tenant Text-Categories Page
+            </Typography>
+        </Grid>
+        <Box sx={{ p: 3 }}>
+            <TextSearch handleParentSearch={handleSearch} />
+            <Grid container spacing={2} sx={{ mt: 2 }}>
+              {filteredCategories.length ? (
+                filteredCategories.map((category) => (
+                <Grid item xs={12} key={category.idCategory}>
+                  <Grid container alignItems="center" spacing={2}>
+                    <Grid item xs={12}>
+                      <TextCategoriesListItem category={category} />
+                    </Grid>
+                </Grid>
+            </Grid>
+        ))
+          ) : (
+            <Grid item xs={12} sx={{ display: "flex", justifyContent: "center", my: 3 }}>
+              <p>No categories found</p>
+            </Grid>
+          )}
+          </Grid>
+        </Box>
+
+        </Grid>
+    </LayoutWrapper>
+    );
 }
