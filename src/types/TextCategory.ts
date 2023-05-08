9ef4cd072910103d43eaafad301a95d2f5fdc3ab
Davide-Milan <davidemilan27@gmail.com>
2023-05-08 23:50:54 +0200

    remove TextCategory type file

--- src/types/TextCategory.ts
+++
@@ -1,9 +0,0 @@
-import Text from './Text';
-
-export default interface TextCategory {
-    idTenant: string,
-    idCategory: string,
-    language: string,
-    isDefault: boolean,
-    List: Text[];
-};
