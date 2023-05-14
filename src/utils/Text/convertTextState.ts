8fa952681aae73ff6b2847db1f597963bf878e60
Gabriel Rovesti <rovestigabriel@gmail.com>
2023-05-14 12:16:48 +0200

    Removed draft state

--- src/utils/Text/convertTextState.ts
+++ src/utils/Text/convertTextState.ts
@@ -1,17 +1,14 @@
-export default function convertTextState(state: string): string {
+export default function convertTextState(state : string) : string{
     switch (state) {
-      case "originalText":
-        return "Original text";
-      case "toBeVerified":
-        return "To be verified";
-      case "verified":
-        return "Verified";
-      case "toBeTranslated":
-        return "To be translated";
-      case "rejected":
-        return "Rejected";
-      default:
-        return "DRAFT";
+        case 'toBeVerified':
+            return 'To be verified';
+        case 'verified':
+            return 'Verified';
+        case 'toBeTranslated':
+            return 'To be translated';
+        case 'rejected':
+            return 'Rejected';
+        default:
+            return 'Original text';
     }
-  }
-  
+}
